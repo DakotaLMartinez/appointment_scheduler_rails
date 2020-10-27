eef839edc00098c10bac04342dbe9bfe56b7d31d
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-10-27 16:45:54 -0700

    add authentication links to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_224851) do
+ActiveRecord::Schema.define(version: 2020_10_27_232444) do
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_10_27_224851) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "patients", "users"
 end
