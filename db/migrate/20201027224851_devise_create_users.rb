631fd170190d0b2724ef9c36df2644aaf0260e57
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-10-27 16:22:01 -0700

    fix problems with devise rememberable

--- db/migrate/20201027224851_devise_create_users.rb
+++ db/migrate/20201027224851_devise_create_users.rb
@@ -9,11 +9,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :phone_number
 
       ## Recoverable
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
-      # ## Rememberable
-      # t.datetime :remember_created_at
+      ## Rememberable
+      t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
--- db/migrate/20201027231345_remove_index_on_reset_password_token_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIndexOnResetPasswordTokenFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_index "users", name: "index_users_on_reset_password_token"
-  end
-end
