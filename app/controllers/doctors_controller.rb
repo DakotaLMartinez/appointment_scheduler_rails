58ccc5a7911aea49da5ded8c891d8bb9d6587a83
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-10-29 16:58:43 -0700

    add doctors controller actions and views

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,36 @@
 class DoctorsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+
+  def index 
+    @doctors = Doctor.all
+  end 
+
+  def show 
+
+  end 
+
+  def new 
+    @doctor = Doctor.new
+  end 
+
+  def create 
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save 
+      redirect_to doctor_path(@doctor)
+    else
+      render :new 
+    end
+  end 
+
+
+  private 
+
+  def set_doctor 
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :phone_number, :specializations)
+  end
 end
